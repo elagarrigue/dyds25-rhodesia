ce45b036875c0f0c3f0b31c7f83bed8fca005dbc
= <prietotiara@gmail.com>
2025-05-26 18:47:12 -0300

    Implementación de caso de uso GetMovieDetailsUseCase.kt para obtener los detalles de película

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/GetMovieDetailsUseCase.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/GetMovieDetailsUseCase.kt
@@ -1,2 +1,10 @@
 package edu.dyds.movies.domain.usecase
 
+import edu.dyds.movies.domain.entity.Movie
+import edu.dyds.movies.domain.repository.MoviesRepository
+
+class GetMovieDetailsUseCase(private val repository: MoviesRepository){
+    suspend operator fun invoke(movieId: Int): Movie {
+        return repository.getMovieDetails(movieId)
+    }
+}
