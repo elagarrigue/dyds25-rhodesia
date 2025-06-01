27ac178b76613c8eb09b5d5f2f86261ad1df9539
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2025-05-31 21:22:37 -0300

    Creación de interfaces PopularMoviesUseCase.kt y MovieDetailsUseCase.kt para aplicar inversión de dependencias con los casos de uso. Cambio de nombre de las clases de caso de uso (remueve Get del nombre). Cambio en la interfaz de las clases useCase, ahora no sobrecargan el método invoke, sino que implementan una nueva llamada execute()

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/MovieDetailsUseCaseImplementation.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/MovieDetailsUseCaseImplementation.kt
@@ -3,8 +3,8 @@ package edu.dyds.movies.domain.usecase
 import edu.dyds.movies.domain.entity.Movie
 import edu.dyds.movies.domain.repository.MoviesRepository
 
-class GetMovieDetailsUseCase(private val repository: MoviesRepository){
-    suspend operator fun invoke(movieId: Int): Movie {
+class MovieDetailsUseCaseImplementation(private val repository: MoviesRepository) : MovieDetailsUseCase {
+    override suspend fun execute(movieId: Int): Movie? {
         return repository.getMovieDetails(movieId)
     }
 }
---
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/PopularMoviesUseCaseImplementation.kt
@@ -0,0 +1,12 @@
+package edu.dyds.movies.domain.usecase
+
+import edu.dyds.movies.domain.entity.QualifiedMovie
+import edu.dyds.movies.domain.repository.MoviesRepository
+
+class PopularMoviesUseCaseImplementation(private val repository: MoviesRepository): PopularMoviesUseCase {
+    override suspend fun execute(): List<QualifiedMovie> {
+        return repository.getPopularMovies().sortedByDescending { it.voteAverage }.map {
+            movie -> QualifiedMovie(movie, movie.voteAverage >= 6.0)
+        }
+    }
+}
