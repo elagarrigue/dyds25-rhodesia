c4c04e7a20ae0c78cb7aea84583f401a6ca9bae4
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2025-07-04 02:44:19 -0300

    Cambios en TMDBMoviesExternalSource.kt - Renombrado a TMDBMoviesExternalSourceImpl.kt - Corrección para que se ajuste al nuevo nombre de método toDomainMovie() en lugar de TMDBtoDomainMovie(). - Corrección en la url de la llamada a la api, que era la causa por la que no funcionaba la aplicación con este origen.

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/tmdb/TMDBMoviesExternalSourceImpl.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/tmdb/TMDBMoviesExternalSourceImpl.kt
@@ -7,21 +7,21 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
-class TMDBMoviesExternalSource(private val tmdbHttpClient: HttpClient) : PopularMoviesExternalSource,
+class TMDBMoviesExternalSourceImpl(private val tmdbHttpClient: HttpClient) : PopularMoviesExternalSource,
     MoviesDetailsExternalSource {
 
     override suspend fun getMovieDetailsFromSource(title : String): Movie? =
         try {
-            getTMDBMovieDetails(title).TMDBtoDomainMovie()
+            getTMDBMovieDetails(title).results.first().toDomainMovie()
         } catch (_: Exception) {
             null
         }
 
     override suspend fun getPopularMoviesFromSource(): List<Movie> =
-        getTMDBPopularMovies().results.map {it. TMDBtoDomainMovie()}
+        getTMDBPopularMovies().results.map { it.toDomainMovie() }
 
-    private suspend fun getTMDBMovieDetails(title: String): TMDBRemoteMovie =
-        tmdbHttpClient.get("/3/movie?query=$title").body()
+    private suspend fun getTMDBMovieDetails(title: String): TMDBRemoteResult =
+        tmdbHttpClient.get("/3/search/movie?query=$title").body()
 
     private suspend fun getTMDBPopularMovies(): TMDBRemoteResult =
         tmdbHttpClient.get("/3/discover/movie?sort_by=popularity.desc").body()
